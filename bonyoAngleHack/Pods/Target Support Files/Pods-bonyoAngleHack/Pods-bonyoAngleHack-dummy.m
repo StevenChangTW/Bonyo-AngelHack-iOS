4eb7453f9078e3d779a0ec99afcdb78cd1caad7c
Michael <helloworldsmart3@gmail.com>
2017-07-29 14:41:14 +0800

    AddFirebase

---
+++ bonyoAngleHack/Pods/Target Support Files/Pods-bonyoAngleHack/Pods-bonyoAngleHack-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bonyoAngleHack : NSObject
+@end
+@implementation PodsDummy_Pods_bonyoAngleHack
+@end
---
+++ bonyoAngleHack/Pods/Target Support Files/Pods-bonyoAngleHackUITests/Pods-bonyoAngleHackUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bonyoAngleHackUITests : NSObject
+@end
+@implementation PodsDummy_Pods_bonyoAngleHackUITests
+@end
